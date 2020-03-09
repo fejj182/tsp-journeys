61dd1db01b8426ad90287478f9fa75d6cb02bafc
Jeff Zinger <jeff.zinger@thoughtworks.com>
2020-03-09 20:01:38 +0100

    Return duration 0 if there is no connection found

--- journeys.js
+++ journeys.js
@@ -3,11 +3,15 @@ const interrail = require('interrail')
 const get = (startId, endId) => {
   return interrail.journeys(startId, endId, {transfers: 0, results: 1})
   .then(response => {
-    const journey = response[0].legs[0]
-    const timeMs = new Date(journey.arrival) - new Date(journey.departure)
-    return {
-      duration: timeMs / 1000 / 60
+    let duration = 0;
+
+    if (response.length > 0) {
+      const journey = response[0].legs[0]
+      const timeMs = new Date(journey.arrival) - new Date(journey.departure)
+      duration = timeMs / 1000 / 60
     }
+
+    return { duration }
   })
 }
 
