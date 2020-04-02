5e6651f56f60e4137df56164bbcf28f0a06c0e3e
Jeff Zinger <jzinger100@gmail.com>
2020-04-02 18:35:51 +0200

    TSP-0: Format on save changes

--- src/journeys.js
+++ src/journeys.js
@@ -1,58 +1,60 @@
-const interrail = require('interrail')
+const interrail = require("interrail");
 
 const getJourneyDuration = (startId, endId) => {
-  return interrail.journeys(startId, endId, {
-    transfers: 0, 
-    results: 1,
-    departureAfter: nextMonday()  
-  })
-  .then(response => {
-    let duration = 0
-    if (response.length > 0) {
-      const journey = response[0].legs[0]
-      const timeMs = new Date(journey.arrival) - new Date(journey.departure)
-      duration = timeMs / 1000 / 60
-    }
-
-    return { duration }
-  })
-}
-
-const captureConnection =  (startId, endId) => {
-  return interrail.journeys(startId, endId, {
-    transfers: 1, 
-    results: 1
-  })
-  .then(response => {
-    if (response.length > 0) {
-      const legs = response[0].legs
-      return {
-        "firstLeg": legs[0], 
-        "secondLeg" : legs[1]
+  return interrail
+    .journeys(startId, endId, {
+      transfers: 0,
+      results: 1,
+      departureAfter: nextMonday()
+    })
+    .then(response => {
+      let duration = 0;
+      if (response.length > 0) {
+        const journey = response[0].legs[0];
+        const timeMs = new Date(journey.arrival) - new Date(journey.departure);
+        duration = timeMs / 1000 / 60;
       }
-    } else {
-      return []
-    }
-  })
-}
+
+      return { duration };
+    });
+};
+
+const captureConnection = (startId, endId) => {
+  return interrail
+    .journeys(startId, endId, {
+      transfers: 1,
+      results: 1
+    })
+    .then(response => {
+      if (response.length > 0) {
+        const legs = response[0].legs;
+        return {
+          firstLeg: legs[0],
+          secondLeg: legs[1]
+        };
+      } else {
+        return [];
+      }
+    });
+};
 
 const nextMonday = () => {
-  const now = new Date(Date.now())
+  const now = new Date(Date.now());
 
   // how many days of the week are left until we reach the same day
-  // change the constant to make it work for any day of the week 
-  const monday = 1
-  let daysLeftTillMonday = (monday + 7 - now.getDay()) % 7
+  // change the constant to make it work for any day of the week
+  const monday = 1;
+  let daysLeftTillMonday = (monday + 7 - now.getDay()) % 7;
 
   if (daysLeftTillMonday === 0) {
-    daysLeftTillMonday += 7
+    daysLeftTillMonday += 7;
   }
 
   now.setDate(now.getDate() + daysLeftTillMonday);
-  return now
-}
+  return now;
+};
 
 module.exports = {
   getJourneyDuration,
   captureConnection
-}
+};
--- tests/journeys.spec.js
+++ tests/journeys.spec.js
@@ -1,79 +1,87 @@
-const journeys = require('../src/journeys')
-const interrail = require('interrail')
+const journeys = require("../src/journeys");
+const interrail = require("interrail");
 
-jest.mock('interrail', () => ({
-    journeys: jest.fn()
-}))
+jest.mock("interrail", () => ({
+  journeys: jest.fn()
+}));
 
-describe('journeys', () => {
-    describe('getJourneyDuration', () => {
-        let nextMonday
-        beforeEach(() => {
-            nextMonday = new Date("February 22, 2016 09:00:00")
-        });
-        it('should call api with correct parameters', async () => {
-            mockResponse()
-            mockDateNow()
-            
-            journeys.getJourneyDuration(123, 456);
-            
-            expect(interrail.journeys).toHaveBeenCalledWith(123, 456, { 
-                transfers: 0, 
-                results: 1,
-                departureAfter: nextMonday
-            });
-        });
-
-        it('should use next monday even if today is monday', async () => {
-            mockResponse()
-            mockDateNow(new Date("February 15, 2016 09:00:00"))
-            
-            journeys.getJourneyDuration(123, 456);
-            
-            expect(interrail.journeys).toHaveBeenCalledWith(123, 456, { 
-                transfers: 0, 
-                results: 1,
-                departureAfter: nextMonday
-            });
-        });
-
-        it('should return duration', async () => {
-            mockResponse()
-            const duration = await journeys.getJourneyDuration(123, 456);
-            expect(duration).toEqual({ duration: 120 })
-        });
-
-        it('should return duration of 0 if response is empty', async () => {
-            mockEmptyResponse()
-            const duration = await journeys.getJourneyDuration(123, 456);
-            expect(duration).toEqual({ duration: 0 })
-        });
+describe("journeys", () => {
+  describe("getJourneyDuration", () => {
+    let nextMonday;
+    beforeEach(() => {
+      nextMonday = new Date("February 22, 2016 09:00:00");
     });
+    it("should call api with correct parameters", async () => {
+      mockResponse();
+      mockDateNow();
+
+      journeys.getJourneyDuration(123, 456);
+
+      expect(interrail.journeys).toHaveBeenCalledWith(123, 456, {
+        transfers: 0,
+        results: 1,
+        departureAfter: nextMonday
+      });
+    });
+
+    it("should use next monday even if today is monday", async () => {
+      mockResponse();
+      mockDateNow(new Date("February 15, 2016 09:00:00"));
+
+      journeys.getJourneyDuration(123, 456);
+
+      expect(interrail.journeys).toHaveBeenCalledWith(123, 456, {
+        transfers: 0,
+        results: 1,
+        departureAfter: nextMonday
+      });
+    });
+
+    it("should return duration", async () => {
+      mockResponse();
+      const duration = await journeys.getJourneyDuration(123, 456);
+      expect(duration).toEqual({ duration: 120 });
+    });
+
+    it("should return duration of 0 if response is empty", async () => {
+      mockEmptyResponse();
+      const duration = await journeys.getJourneyDuration(123, 456);
+      expect(duration).toEqual({ duration: 0 });
+    });
+  });
+
+  describe("captureConnection", () => {
+    // TODO: write tests
+  });
 });
 
 const mockResponse = () => {
-    interrail.journeys.mockImplementation(jest.fn(() => Promise.resolve([
+  interrail.journeys.mockImplementation(
+    jest.fn(() =>
+      Promise.resolve([
         {
-            legs: [{
-                departure: "2020-03-22T16:15:00.000+01:00",
-                arrival: "2020-03-22T18:15:00.000+01:00", 
-            }]
+          legs: [
+            {
+              departure: "2020-03-22T16:15:00.000+01:00",
+              arrival: "2020-03-22T18:15:00.000+01:00"
+            }
+          ]
         }
-    ])))
-}
+      ])
+    )
+  );
+};
 
 const mockDateNow = (now = null) => {
-    if (!now) {
-        now = new Date("February 15, 2016 09:00:00");
-        const zeroToSix = Math.floor(Math.random() * 7);
-        now.setDate(now.getDate() + zeroToSix); 
-    }
+  if (!now) {
+    now = new Date("February 15, 2016 09:00:00");
+    const zeroToSix = Math.floor(Math.random() * 7);
+    now.setDate(now.getDate() + zeroToSix);
+  }
 
-    jest
-        .spyOn(global.Date, 'now')
-        .mockImplementationOnce(() => now.valueOf() );
-}
+  jest.spyOn(global.Date, "now").mockImplementationOnce(() => now.valueOf());
+};
 
 const mockEmptyResponse = () => {
-    interrail.journeys.mockImplementation(jest.fn(() => Promise.resolve([])))
-}
+  interrail.journeys.mockImplementation(jest.fn(() => Promise.resolve([])));
+};
